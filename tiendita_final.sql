7e8ecb86bcb50e86b241208c9961a5a94b8236c1
jvaldemar87 <32584257+jvaldemar87@users.noreply.github.com>
2018-07-31 01:36:35 -0500

    actualizado al 31-07-2018
    0136 am

--- tiendita_final.sql
+++ tiendita_final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 25-07-2018 a las 18:44:08
+-- Tiempo de generación: 31-07-2018 a las 08:10:35
 -- Versión del servidor: 10.1.31-MariaDB
 -- Versión de PHP: 7.2.3
 
@@ -21,6 +21,33 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `tiendita_final`
 --
+
+DELIMITER $$
+--
+-- Procedimientos
+--
+CREATE DEFINER=`root`@`localhost` PROCEDURE `agregar_producto` (`prod_nombre` VARCHAR(50), `prod_precio` FLOAT, `prod_costo` FLOAT, `prod_descuento` FLOAT)  INSERT INTO producto
+(
+    `id`,
+    `nombre`,
+    `precio`,
+    `costo`,
+    `descuento`,
+    `fecha_inser`,
+    `fecha_modif`
+)
+VALUES
+(
+    NULL,
+    `prod_nombre`,
+    `prod_precio`,
+    `prod_costo`,
+    `prod_descuento`,
+    NOW(),
+    NOW()
+)$$
+
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -50,10 +77,23 @@ INSERT INTO `producto` (`id`, `nombre`, `precio`, `costo`, `descuento`, `fecha_i
 (5, 'papas chile', 30.4, 24.87, 0.76, '2018-07-19 04:16:06', '2018-07-19 04:16:06'),
 (6, 'huuh', 36.23, 32.42, 24, '2018-07-19 04:20:37', '2018-07-19 04:20:37'),
 (7, 'fgfgfg', 87.32, 70.63, 10.4, '2018-07-19 04:34:03', '2018-07-19 04:34:03'),
-(8, 'lñlñlñlñlñ', 87.32, 76.35, 3.53, '2018-07-19 04:34:43', '2018-07-19 04:34:43'),
+(8, 'lñlñlñlñlñ', 97.32, 76.35, 3.53, '2018-07-19 04:34:43', '2018-07-19 04:34:43'),
 (9, 'opopopopop', 48.3, 39.14, 19.4, '2018-07-19 04:37:39', '2018-07-19 04:37:39'),
 (11, 'tunas', 84.65, 42.46, 12.37, '2018-07-19 09:36:11', '2018-07-20 16:21:41'),
 (12, 'platanos', 69.25, 40.61, 0, '2018-07-24 06:36:11', '2018-07-24 20:48:50');
+
+--
+-- Disparadores `producto`
+--
+DELIMITER $$
+CREATE TRIGGER `cambia_precio` AFTER UPDATE ON `producto` FOR EACH ROW insert INTO tabla_cambios set
+id = null,
+id_producto = OLD.id,
+valor_ant = OLD.precio,
+valor_nvo = NEW.precio,
+fecha_modif = NOW()
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -69,6 +109,13 @@ CREATE TABLE `tabla_cambios` (
   `fecha_modif` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Volcado de datos para la tabla `tabla_cambios`
+--
+
+INSERT INTO `tabla_cambios` (`id`, `id_producto`, `valor_ant`, `valor_nvo`, `fecha_modif`) VALUES
+(1, 8, '87.31999969482422', '97.31999969482422', '2018-07-30 22:47:28');
+
 -- --------------------------------------------------------
 
 --
@@ -81,6 +128,27 @@ CREATE TABLE `vendidos` (
   `cantidad` int(11) NOT NULL,
   `fecha_inser` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `vendidos`
+--
+
+INSERT INTO `vendidos` (`id`, `id_producto`, `cantidad`, `fecha_inser`) VALUES
+(1, 2, 140, '2018-07-25 10:23:13'),
+(2, 4, 40, '2018-07-25 10:24:11'),
+(3, 5, 646, '2018-07-25 10:57:20'),
+(4, 1, 25, '2018-07-25 11:40:48'),
+(5, 2, 6, '2018-07-25 11:43:03'),
+(6, 12, 419, '2018-07-25 11:46:48'),
+(7, 11, 143, '2018-07-25 11:47:07'),
+(8, 9, 455, '2018-07-25 11:47:28'),
+(9, 8, 421, '2018-07-25 11:47:38'),
+(10, 7, 236, '2018-07-25 11:47:47'),
+(11, 6, 872, '2018-07-25 11:47:58'),
+(12, 5, 951, '2018-07-25 11:48:09'),
+(13, 4, 349, '2018-07-25 11:48:19'),
+(14, 3, 46, '2018-07-25 11:48:27'),
+(15, 3, 245, '2018-07-25 11:48:46');
 
 --
 -- Índices para tablas volcadas
@@ -118,13 +186,13 @@ ALTER TABLE `producto`
 -- AUTO_INCREMENT de la tabla `tabla_cambios`
 --
 ALTER TABLE `tabla_cambios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT de la tabla `vendidos`
 --
 ALTER TABLE `vendidos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
